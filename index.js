a7f5971a122e0acf4ad86310e1da02d7ff49b28d
VIB-source <61460481+VIB-source@users.noreply.github.com>
2024-09-04 11:33:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import cfonts from 'cfonts';
 
 const { say } = cfonts;
 
-say("XLICON - V2", {
+say("DoOmSss-BoT", {
   'font': "block",
   'align': "center",
   'colors': ['#ff9900'],
@@ -21,7 +21,7 @@ say("XLICON - V2", {
   'maxLength': '15'
 });
 
-say("Xlicon-BOT-V2 By Salman._.", {
+say("DoOmSss-BOT By DoOmSss._.", {
   'font': "chrome",
   'align': "center",
   'colors': ["red", "magenta"],
